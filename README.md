8a84c0171426da394bc5419f80a46b25bf0029e4
slnishi <93013772+ShaiyaJ@users.noreply.github.com>
2026-04-04 06:51:33 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# snob
+The **S**imple **Nob**uild system for C.
+
+## About
+Snob is a bulid tool for C that is inspired by [nob](https://github.com/tsoding/nob.h). It is a STB-style single header file library, making it easy to include in your project.
+
+The premise behind the library is that you shouldn't need an external build tool in order to build C programs. Instead, you should be able to build an entire C project with just the C compiler. 
+
+### How does it work? 
+**S**nob is deliberately simple. It compiles each file twice - the first building the second.
+
+It achieves this by (re)defining a `main` function. 
+
+During the first build the (re)defined main gets compiled. This (re)defined main then invokes the command line through `snob_cmd` or `snob_build`. When this executable is run it will contain instructions on how to build itself.
+
+## Use
+### A word of warning
+1. This library is very experimental and things are due to change drastically. 
+2. I'm largely creating this to see if this is a viable way to build C projects. This was originally inteded to build super simple project, and will probably be very slow or headache-inducing to use in larger projects with more complicated dependencies. It also **doesn't support incremental builds** or **building itself**... (yet)
+
+All in all... **don't use this**. Unless you really want to.
+
+### How to use snob
+Snob is fairly easy to use. Here's a reference guide:
+
+#### `#include "snob.h"` 
+Renames the `main` function to `_snob_prog_main`. 
+
+It also `#define`s some constants like `SNOB_CC` and `SNOB_CFLAGS`.
+
+#### `snob_start()` / `snob_main()`
+[warning, due to change very soon]
+
+Defines the start of the build logic.
+
+#### `snob_end()`
+Defines the end of the build logic.
+
+#### `snob_cmd(...)`
+This runs a command. It takes a list of strings and concats them into a single argument. It doesn't perform any checks on what "should" be grouped or not.
+
+Currently the default for this is to use `system()` from `stdlib.h`. But, you can redefine this before the include if you wish to provide your own functionality.
+
+#### `snob_build(extras)`
+This is a wrapper around `snob_cmd`. It uses `__FILE__`, `SNOB_CFLAGS` and `extras` as the argv to `SNOB_CC`.
+
+Similar to `snob_cmd`, it can be redefined before you include snob to allow for your own logic.
+
+#### `snob_nob()`
+"Snob no-build". Is equivalent to calling `snob_start` and `snob_end` with no logic inbetween. In other words, "don't do anything".
--- snob.h
+++ snob.h
@@ -1,41 +1,14 @@
 #ifndef H_SNOB
 #define H_SNOB
 
-/*
-#include <stdlib.h>
-
 // Overriding main
-#define main            snob_prog_main
+#define main(...)   main(void) {            \
+                        _snob_build_fn();   \
+                    }                       \
+                    int _snob_prog_main(__VA_ARGS__)
 
-// Default flags and constants
-#ifndef SNOB_CC
-    #define SNOB_CC     "cc"
-#endif
 
-#ifndef SNOB_CFLAGS
-    #define SNOB_CFLAGS ""
-#endif
-
-#ifdef SNOB_POLLUTE_ALIAS
-    #define CC      SNOB_CC
-    #define CFLAGS  SNOB_CFLAGS
-#endif
-
-// Build script
-#ifdef _WIN32
-    // TODO
-#else
-    #define snob_build(...)     system(SNOB_CC " " SNOB_CFLAGS " " __VA_ARGS__ " " __FILE__)
-#endif
-
-// Redefining main
-#define snob_build_start()      int main(int argc, char** argv) {
-#define snob_build_end()            snob_build();    \
-
-*/
-
-// Default flag values
-
+// Parameters
 #ifndef SNOB_CC
 #   define SNOB_CC "cc" 
 #endif
@@ -44,87 +17,22 @@
 #   define SNOB_CFLAGS ""
 #endif
 
-// Default command handling
-
-#ifndef snob_cmd
+// Running commands
+#ifndef snob_cmd        
 #   include <stdlib.h>
-#   define snob_cmd(...)            system(__VA_ARGS__)
+#   define snob_cmd(...)            system(__VA_ARGS__);
 #endif
 
+// Predefined commands
 #ifndef snob_build
 #   define snob_build(extras)       snob_cmd(SNOB_CC " " SNOB_CFLAGS " " extras " " __FILE__)
 #endif
 
-// Implementation details
-char* snob_process_source_file(const char* target);     // Reads content of file and prepends "#if 0" and appends #endif to comment out non-build related code
+// Course of a snob program's life
+#define snob_start()    void _snob_build_fn(void) {
+//#define snob_main()     int main(void) {return 0;} snob_start()
+#define snob_end()      }
 
-#define SNOB_BUILD_PATH "./snob_build.c"
-#define SNOB_HEADER_PATH "./snob.h"
-FILE* snob_generate_build_file();                       // Generates the build file name depending on SNOB_BUILD_PATH
-
-// ---- Main implementation ----
-
-#ifdef SNOB_IMPLEMENTATION
-
-#include <stdlib.h>
-
-char* snob_process_source_file(const char* target) {
-    // Opening target file
-    FILE* source_file = fopen(target, "w+");
-    //FILE* temp_file = fopen();
-    //
-    // // Writing header
-    // const char* HEADER = "#if 0\n";
-    // fwrite(sizeof(HEADER), 1, HEADER, temp_file);
-    //
-    // // Copying source file over to temp
-    // int c;
-    // while ( (c = fgetc(source_file) != EOF) )
-    //      fputc(c, temp_file);
-    //
-    // // Writing footer
-    // const char* FOOTER = "#endif\n";
-    // fwrite(sizeof(FOOTER), 1, FOOTER, temp_file);
-
-    if (build_file == NULL)
-        goto error;
-
-    // Closing target file
-    fclose(source_file);
-
-    // Error handling
-    error:
-        return NULL;
-}
-
-FILE* snob_generate_build_file() {
-    // Creating temporary file for program to use
-    FILE* build_file = fopen(SNOB_BUILD_PATH, "w");
-
-    if (build_file == NULL)
-        goto error;
-
-    // Creating build
-    const char* HEADER = "#include \"" SNOB_HEADER_PATH   "\"\n";
-    fwrite(sizeof(HEADER), 1, HEADER, build_file);
-
-    // for (file : ./*.c) {
-    //      const char* INCLUDE = "#include \"";
-    //      char* temp = snob_process_source_file(target);
-    //      const char* INCLUDEEND = "#\"\n";
-    //
-    //      fwrite(sizeof(INCLUDE), 1, INCLUDE, build_file);
-    //      fwrite(strlen(temp), 1, temp, build_file);
-    //      fwrite(sizeof(INCLUDEEND), 1, INCLUDEEND, build_file);
-    // }
-    
-    // Error handling
-    error: 
-        return NULL;
-}
-
-#endif
-
-// -----------------------------
+#define snob_nob()      void _snob_build_fn(void) {}
 
 #endif
